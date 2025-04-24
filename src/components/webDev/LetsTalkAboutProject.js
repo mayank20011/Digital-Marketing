b693c72f8587e0b9d523e9046907d554bd5cd6a8
mayank20011 <mayank.behl.mb1@gmail.com>
2025-04-24 17:17:55 +0530

    added &apos; in lets talk about project component

--- src/components/webDev/LetsTalkAboutProject.js
+++ src/components/webDev/LetsTalkAboutProject.js
@@ -3,7 +3,7 @@ export default function LetsTalkAboutProject(){
   return (
     <div className="flex flex-col gap-6 md:gap-12 custom-container text-center pt-12">
       <h1 className="text-2xl lg:text-4xl xl:text-6xl font-bold">LET'S TALK ABOUT PROJECT</h1>
-      <p>Let’s bring it to life together. Whether you're starting from scratch or need expert help scaling your existing product, we’re here to understand your goals and turn them into a powerful digital solution—designed for impact, growth, and real results.</p>
+      <p>Let&apos;s bring it to life together. Whether you're starting from scratch or need expert help scaling your existing product, we&apos;re here to understand your goals and turn them into a powerful digital solution—designed for impact, growth, and real results.</p>
       <Link href="/contact-us" className="text-white bg-gradient-to-tr w-fit  mx-auto px-4 py-2 rounded-full hover:bg-gradient-to-tl transition duration-300 from-orange-300 to-pink-600 hover:shadow-[3px_3px_1px_black] hover:-translate-y-[10px] border">Contact Us</Link>
     </div>
   );
