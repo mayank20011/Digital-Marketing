017443655a5beff3853be2017965c8be6ec3969e
mayank20011 <mayank.behl.mb1@gmail.com>
2025-04-25 17:39:40 +0530

    .

--- src/components/DigitalMarketing/WhyUsDigitalMarketing.js
+++ src/components/DigitalMarketing/WhyUsDigitalMarketing.js
@@ -4,7 +4,7 @@ export default function WhyUsDigitalMarketing(){
     <div className="custom-container flex flex-col lg:flex-row">
       <div className="w-full lg:w-1/2 bg-gradient-to-r from-white to-orange-300 rounded-tr-[50px] p-12 pr-16 flex flex-col gap-12 items-center justify-center">
         <h1 className="text-4xl font-bold text-black">Why Vardaan Farms As Your Digital Marketing Team</h1>
-        <p className="">At Vardaan Farms, we go beyond buzzwords and vanity metrics. We focus on strategies that drive real results—whether it's building your brand, generating leads, or boosting conversions. Our team treats your goals like our own, blending creativity with data to help your business grow with purpose and impact.</p>
+        <p className="">At Vardaan Farms, we go beyond buzzwords and vanity metrics. We focus on strategies that drive real results—whether it&apos;s building your brand, generating leads, or boosting conversions. Our team treats your goals like our own, blending creativity with data to help your business grow with purpose and impact.</p>
       </div>
       <div className="w-full lg:w-1/2 h-full lg:pt-[50px] pb-[20px] lg:-translate-x-[50px] -translate-y-[30px] lg:-translate-y-0">
          <div className="bg-white h-[500px] rounded-tl-[50px] shadow-2xl mx-2 sm:mx-4 md:mx-6 lg:mx-0">
