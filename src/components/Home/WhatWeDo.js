cee1abae3cc2ab3355d3735f0fe852f5ba0969db
mayank20011 <mayank.behl.mb1@gmail.com>
2025-05-03 13:03:59 +0530

    changed order in home cards of what we do

--- src/components/Home/WhatWeDo.js
+++ src/components/Home/WhatWeDo.js
@@ -8,16 +8,16 @@ export default function WhatWeDo() {
         {whatWeDo.map((data,id) => (
           <Link key={id} href={data.href}>
             <div
-              className={`h-full flex items-center gap-2 sm:gap-6 p-2 sm:p-4 rounded-2xl border-2 border-b-8 border-black hover:scale-102 transition flex-row sm:flex-col lg:flex-row cursor-pointer ${
+              className={`h-full flex items-center gap-2 sm:gap-6 p-2 py-4 sm:p-4 rounded-2xl border-2 border-b-8 border-black hover:scale-102 transition flex-row sm:flex-col lg:flex-row cursor-pointer ${
                 data.bg == "black"
                   ? "bg-black text-white border-slate-200"
                   : "bg-white text-black"
               }`}
             >
-              <div className={""}>
+              <div className={`sm:order-1 ${(id+1)%2==0? 'order-2' : ''}`}>
                 <img src="/digitalMarketing.webp" alt="" />
               </div>
-              <div className="flex flex-col gap-2 justify-center">
+              <div className={` sm:order-2 flex flex-col gap-2 justify-center`}>
                 <h1 className="sm:text-lg md:text-xl lg:text-2xl font-bold">
                   {data.heading}
                 </h1>
