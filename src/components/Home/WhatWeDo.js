50b909e143556fbc13efaa0b9d80f60a2b80c2b0
mayank20011 <mayank.behl.mb1@gmail.com>
2025-05-03 12:56:14 +0530

    changed what we do cards

--- src/components/Home/WhatWeDo.js
+++ src/components/Home/WhatWeDo.js
@@ -4,11 +4,11 @@ export default function WhatWeDo() {
   return (
     <div className="custom-container flex flex-col gap-6 md:gap-12">
       <h2 className="text-4xl text-center font-bold">What We do ?</h2>
-      <div className="w-full grid md:grid-cols-2 gap-6">
+      <div className="w-full grid sm:grid-cols-2 gap-6">
         {whatWeDo.map((data,id) => (
           <Link key={id} href={data.href}>
             <div
-              className={`h-full flex items-center gap-6 p-4 sm:p-6 rounded-2xl border-2 border-b-8 border-black hover:scale-102 transition flex-col lg:flex-row cursor-pointer ${
+              className={`h-full flex items-center gap-2 sm:gap-6 p-2 sm:p-4 rounded-2xl border-2 border-b-8 border-black hover:scale-102 transition flex-row sm:flex-col lg:flex-row cursor-pointer ${
                 data.bg == "black"
                   ? "bg-black text-white border-slate-200"
                   : "bg-white text-black"
@@ -17,13 +17,13 @@ export default function WhatWeDo() {
               <div className={""}>
                 <img src="/digitalMarketing.webp" alt="" />
               </div>
-              <div className="flex flex-col gap-2 sm:gap-4 justify-center">
-                <h1 className="text-lg sm:text-xl md:text-2xl font-bold">
+              <div className="flex flex-col gap-2 justify-center">
+                <h1 className="sm:text-lg md:text-xl lg:text-2xl font-bold">
                   {data.heading}
                 </h1>
-                <p className="text-sm md:text-md">{data.pera}</p>
+                <p className="text-xs sm:text-sm">{data.pera}</p>
                 <p
-                  className={`text-sm md:text-md w-fit px-4 py-2 rounded-md scale-105 transform ${
+                  className={`text-xs md:text-sm w-fit px-2 py-1 sm:px-4 sm:py-2 rounded-md scale-105 transform ${
                     data.bg == "black"
                       ? "hover:bg-white hover:text-black"
                       : "hover:bg-black hover:text-white"
